e0374c772ad588175382f3160163149277f0ba88
Marcos Vinicius <mvplemes@gmail.com>
2024-10-20 23:09:52 -0300

    cadastrar grupo

--- grupos.py
+++ grupos.py
@@ -28,3 +28,29 @@ def registrar_rotas_grupos(app, cursor):
             })
         else:
             return jsonify({'message': 'Grupo não encontrada'}), 404
+        
+    @app.route('/grupos', methods=['POST'])
+    def cadastrarGrupo():
+        try:
+            cursor.execute("INSERT INTO public.grupo (gp_nome,gp_mostrar_foto) VALUES(%s, %s) RETURNING gp_codigo",
+                        (request.form['descricao'],
+                            request.form['isMostrarImagem']))
+        
+            codigo = cursor.fetchone()[0]
+            return jsonify({
+                'message': 'Grupo cadastrada com sucesso!',
+                'codigo': codigo
+                }), 201    
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
+        
+    @app.route('/grupos', methods=['PUT'])
+    def alterarGrupo():
+        try:
+            cursor.execute("UPDATE public.grupo SET gp_mostrar_foto=%s, gp_nome=%s WHERE gp_codigo=%s;",
+                        (request.form['descricao'],
+                         request.form['isMostrarImagem'],
+                         request.form['codigo']))
+            return jsonify({'message': 'Grupo Alterado com sucesso!'}), 201    
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500

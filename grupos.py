def7425498365b798ce457f41f68e02a2ea90637
Marcos Vinicius <mvplemes@gmail.com>
2024-10-20 22:38:57 -0300

    buscar grupo

--- grupos.py
+++ grupos.py
@@ -3,7 +3,7 @@ import base64
 
 def registrar_rotas_grupos(app, cursor):
     @app.route('/grupos')
-    def getGrupos():
+    def get_grupos():
         cursor.execute("SELECT * FROM grupo")
         grupos = list()
         for grupo in cursor.fetchall():
@@ -14,3 +14,17 @@ def registrar_rotas_grupos(app, cursor):
             })
     
         return grupos
+    
+    @app.route('/grupos/<int:codigo>', methods=['GET'])
+    def get_grupo(codigo):
+        cursor.execute("SELECT * FROM grupo WHERE gp_codigo = %s", (codigo,))
+        grupo = cursor.fetchone()  
+
+        if grupo:
+            return jsonify({
+                'codigo': grupo[0],
+                'nome': grupo[1],
+                'imaisMostrarFotogem': grupo[2]
+            })
+        else:
+            return jsonify({'message': 'Grupo não encontrada'}), 404

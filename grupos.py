05696f14a7e2c0b18185fcf9774984fc23baf048
Marcos Vinicius <mvplemes@gmail.com>
2024-10-21 00:10:25 -0300

    aaa

--- grupos.py
+++ grupos.py
@@ -3,9 +3,9 @@ import base64
 
 def registrar_rotas_grupos(app, cursor):
     @app.route('/grupos/<int:codigo>', methods=['DELETE'])
-        def deletarGrupo(codigo):
-            cursor.execute("DELETE FROM grupo WHERE gp_codigo = %s", (codigo,))
-            return jsonify({'message': 'Grupo deletada com sucesso!'}), 200
+    def deletarGrupo(codigo):
+        cursor.execute("DELETE FROM grupo WHERE gp_codigo = %s", (codigo,))
+        return jsonify({'message': 'Grupo deletada com sucesso!'}), 200
 
     @app.route('/grupos')
     def get_grupos():
